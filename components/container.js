7591cb296f412c4848141c23f32a84dbd254c8da
Angel Paunchev <apaunchev@gmail.com>
2022-05-28 10:17:24 +0300

    Add title

--- components/container.js
+++ components/container.js
@@ -1,12 +1,18 @@
+import Head from 'next/head';
 import { Nav } from './nav';
 
 export function Container({ children }) {
   return (
-    <div className="container max-w-6xl mx-auto px-4 py-12">
-      <div className="flex flex-col gap-4">
-        <Nav />
-        {children}
+    <>
+      <Head>
+        <title>Salaree</title>
+      </Head>
+      <div className="container max-w-6xl mx-auto px-4 py-12">
+        <div className="flex flex-col gap-4">
+          <Nav />
+          {children}
+        </div>
       </div>
-    </div>
+    </>
   );
 }
